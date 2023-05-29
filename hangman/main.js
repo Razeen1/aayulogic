a1f3474cbc30583a2b31f891f6ce985e6d882b9d
rajin <rz.bhuju12@gmail.com>
2023-05-29 12:43:49 +0545

    Functions display typed words added

--- hangman/main.js
+++ hangman/main.js
@@ -1,9 +1,14 @@
-let arr=["Hello","qwrt"];
-let pressedArr=[];
+const arr=["Hello","qwrt"];
+const pressedArr=[];
 document.addEventListener('keypress', (event) => {
-  var name = event.key;
-  var code = event.code;
-  keyCheck(name,1);
+    var name = event.key;
+    var charCode=event.which;
+    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)){
+        keyCheck(name,0);
+    }
+ 
+
+  
 }, false);
 let divMain=div();
 document.body.appendChild(divMain);
@@ -35,29 +40,30 @@ function keyCheck(x,y){
     const myArray = arr[y].split('');
     for(i=0;i < myArray.length;i++){
         if(myArray[i].toLowerCase()===x.toLowerCase()){
-            alert("present");
             var a=1;
-            displayWrong(x);
             break;
         };
     };
     if(a!==1){
         alert("Not present");
         displayWrong(x);
-    };      
+    }else {
+        displayWrong(x);
+        alert("present");
+    }      
 }
 function displayWrong(wrong){
-    for(i=0;i=pressedArr.length;i++){
-        if(pressedArr==wrong){
+    for(i=0;i<pressedArr.length;i++){
+        if(pressedArr[i]==wrong){
             var a=1;
             break;
         }
         
-    }if(a!=1){
+    }
+    if(a!=1){
         let textNodeh = document.createTextNode(wrong);
         txtP.appendChild(textNodeh);
         divMain.appendChild(txtP);
         pressedArr.push(wrong);
     }
-    
 }
