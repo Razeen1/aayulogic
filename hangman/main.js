36c82c22f5e4ffc204ef7feb3f72b56225b64fe0
rajin <rz.bhuju12@gmail.com>
2023-05-31 11:44:48 +0545

    Minor changes

--- hangman/main.js
+++ hangman/main.js
@@ -1,14 +1,14 @@
-const arr = ["banana", "apple","halloween","bottle","laptop"];
+const arr = ["banana", "apple", "halloween", "bottle", "laptop"];
 
-var lives=6;
+var lives = 6;
 
 let divMain = div();
 
-var topdiv=document.getElementById("topDiv");
-var main=document.getElementById("main");
+var topdiv = document.getElementById("topDiv");
+var main = document.getElementById("main");
 const h1 = document.createElement("h1");
 const textNode = document.createTextNode("Hang Man");
-h1.style.textAlign="center";
+h1.style.textAlign = "center";
 h1.appendChild(textNode);
 topdiv.parentNode.insertBefore(h1, topdiv);
 topdiv.appendChild(divMain);
@@ -29,13 +29,13 @@ var txtPS = txtShowPS();
 
 divMain.appendChild(buttonC);
 
-function startGame(){
-    document.querySelector('.start').style.display="none";
-    selected=Math.floor(Math.random() * arr.length);
+function startGame() {
+    document.querySelector('.start').style.display = "none";
+    selected = Math.floor(Math.random() * arr.length);
     document.addEventListener('keydown', (event) => {
         start(event);
     }
-    , false);
+        , false);
     buttonDiv.appendChild(buttonH);
     buttonDiv.appendChild(buttonR);
     divMain.appendChild(txtP);
@@ -43,65 +43,66 @@ function startGame(){
     divMain.appendChild(txtC);
     document.body.appendChild(txtPS);
 
-    displayLine();
+    displayLines();
     canvas();
     drawArray[9]();
     drawArray[8]();
     drawArray[7]();
 }
-function start(event){
+function start(event) {
     var name = event.key;
     var charCode = event.which;
     if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)) {
-        keyCheck(name, selected);}
+        keyCheck(name, selected);
+    }
 }
 
-function createButton(){
+function createButton() {
     let buttonC = document.createElement('button');
     buttonC.classList.add("start");
     buttonC.style.border = 'none';
-    buttonC.style.borderRadius= '5px';
+    buttonC.style.borderRadius = '5px';
     buttonC.style.background = 'black';
     buttonC.style.color = 'white';
     buttonC.style.padding = '10px';
     buttonC.style.margin = '10px';
     buttonC.innerHTML = 'Start Game';
-    buttonC.onclick=function() { startGame(); };
+    buttonC.onclick = function () { startGame(); };
     return buttonC;
 }
 
-function createButton2(){
+function createButton2() {
     let buttonC = document.createElement('button');
     buttonC.classList.add("hint");
     buttonC.style.border = 'none';
-    buttonC.style.borderRadius= '5px';
+    buttonC.style.borderRadius = '5px';
     buttonC.style.background = 'black';
     buttonC.style.color = 'white';
     buttonC.style.padding = '10px';
     buttonC.style.margin = '10px';
     buttonC.innerHTML = 'Hint';
-    buttonC.onclick=function() { hint(); };
+    buttonC.onclick = function () { hint(); };
     return buttonC;
 }
 
-function createButton3(){
+function createButton3() {
     let buttonC = document.createElement('button');
     buttonC.classList.add("reset");
     buttonC.style.border = 'none';
-    buttonC.style.borderRadius= '5px';
+    buttonC.style.borderRadius = '5px';
     buttonC.style.background = 'black';
     buttonC.style.color = 'white';
     buttonC.style.padding = '10px';
     buttonC.style.margin = '10px';
     buttonC.innerHTML = 'Play Again';
-    buttonC.onclick=function() { endGame(); };
+    buttonC.onclick = function () { endGame(); };
     return buttonC;
 }
 
 var animate = function () {
-    var drawMe = lives ;
+    var drawMe = lives;
     drawArray[drawMe]();
-    
+
 }
 
 function div() {
@@ -127,6 +128,7 @@ function div2() {
 function txtShow() {
     const pText = document.createElement('p');
     pText.classList.add("textP");
+
     pText.style.display = 'flex';
     pText.style.alignItems = 'center';
     return pText;
@@ -144,14 +146,15 @@ function txtShowLine() {
     const pText = document.createElement('p');
     pText.classList.add("textC");
     pText.style.display = 'flex';
+    pText.style.color = 'red';
     pText.style.alignItems = 'center';
     return pText;
 }
+
 function txtShowPS() {
     const pText = document.createElement('p');
     pText.classList.add("textPS");
     const textNode = document.createTextNode("Press any key to guess the word");
-    
     pText.style.textAlign = 'center';
     pText.appendChild(textNode);
     return pText;
@@ -162,32 +165,34 @@ function inputAnswer() {
     ansInput.style.display = 'hidden';
 
 }
+
 function keyCheck(x, y) {
     const myArray = arr[y].split('');
-    a=0;
+    a = 0;
     for (i = 0; i < myArray.length; i++) {
         if (myArray[i].toLowerCase() === x.toLowerCase()) {
             var a = 1;
             break;
-            
+
         };
     };
     if (a !== 1) {
-        document.querySelector(".textC").innerHTML="Not Present";
-        setTimeout(function() {
-            document.querySelector(".textC").innerHTML="";
-        
-          }, 700);
-        displayWrong(x,1);
-        
+        document.querySelector(".textC").innerHTML = "Not Present";
+        setTimeout(function () {
+            document.querySelector(".textC").innerHTML = "";
+
+        }, 700);
+        displayWrong(x, 1);
+
     } else {
-        document.querySelector(".textC").innerHTML="";
+        document.querySelector(".textC").innerHTML = "";
         displayR(x);
-        displayWrong(x,2);
-        
+        displayWrong(x, 2);
+
     }
 }
-function displayWrong(wrong,qw) {
+
+function displayWrong(wrong, qw) {
     for (i = 0; i < pressedArr.length; i++) {
         if (pressedArr[i] == wrong) {
             var a = 1;
@@ -195,48 +200,46 @@ function displayWrong(wrong,qw) {
         }
     }
     if (a != 1) {
-        let textNodeh = document.createTextNode(wrong.toUpperCase()+" ");
+        let textNodeh = document.createTextNode(wrong.toUpperCase() + " ");
         txtP.appendChild(textNodeh);
         divMain.appendChild(txtP);
         pressedArr.push(wrong);
-        if(qw!==2){
+        if (qw !== 2) {
             console.log(lives);
-        if(lives!=-1){
-            animate();
-            lives-=1;
-        }
+            if (lives != -1) {
+                animate();
+                lives -= 1;
+            }
             else {
-                document.querySelector(".textR").innerHTML="You lose! The word was: "+arr[selected].toUpperCase();
-                setTimeout(function() {
+                document.querySelector(".textR").innerHTML = "You lose! The word was: " + arr[selected].toUpperCase();
+                setTimeout(function () {
                     endGame();
-                
-                  }, 2000);
-                   
-                 
+
+                }, 2000);
+
+
             }
         }
     }
 }
-function displayLines(){
-    let selectedNum=arr[selected];
-    correct = document.createElement('ul');
-    correct.style.display="flex";
 
+function displayLines() {
+    let selectedNum = arr[selected];
+    correct = document.createElement('ul');
+    correct.style.display = "flex";
     for (var i = 0; i < selectedNum.length; i++) {
         correct.setAttribute('id', 'myword');
         guess = document.createElement('li');
-        
+
         // guess.setAttribute('class', 'guess');
         guess.innerHTML = "_";
         divMain.appendChild(correct);
-      correct.appendChild(guess);
+        correct.appendChild(guess);
     }
 }
-function displayLine(l){
-        displayLines();
-}
-function displayR(lq){
-    let selectedWord=arr[selected].split('');
+
+function displayR(lq) {
+    let selectedWord = arr[selected].split('');
     for (i = 0; i < correctArr.length; i++) {
         if (pressedArr[i] === lq) {
             var ad = 1;
@@ -246,147 +249,160 @@ function displayR(lq){
     if (ad != 1) {
         var indices = [];
         var li = document.querySelectorAll("ul > li");;
-        
-        for(var i=0; i<selectedWord.length;i++) {
-        if (selectedWord[i] == lq){
-            indices.push(i);
-        } 
-        }
-        for(var i=0; i<indices.length;i++) {
-            correctArr[indices[i]]=lq;
-            li[indices[i]].innerHTML=lq.toUpperCase();
-            
+
+        for (var i = 0; i < selectedWord.length; i++) {
+            if (selectedWord[i] == lq) {
+                indices.push(i);
             }
+        }
+        for (var i = 0; i < indices.length; i++) {
+            correctArr[indices[i]] = lq;
+            li[indices[i]].innerHTML = lq.toUpperCase();
+
+        }
     }
     finishGameWin();
-        
+
 }
-function finishGameWin(){
-    let selectedNum=arr[selected].split('');
-    if(correctArr.toString()==selectedNum.toString()){
+function finishGameWin() {
+    let selectedNum = arr[selected].split('');
+    if (correctArr.toString() == selectedNum.toString()) {
         const controller = new AbortController();
-        setTimeout(function() {
+        setTimeout(function () {
             // document.querySelector(".textC").innerHTML="";
-            document.querySelector(".textR").innerHTML="Correct! You Win!";
-            setTimeout(function() {
+            document.querySelector(".textR").innerHTML = "Correct! You Win!";
+            setTimeout(function () {
                 endGame();
-            
-              }, 2000);
-          }, 500);
+
+            }, 2000);
+        }, 500);
     }
 }
-function endGame(){
-   
+function endGame() {
+
     document.removeEventListener('keydown', (event) => {
         start(event);
     }
-    , false);
+        , false);
     pressedArr = [];
     correctArr = [];
-    document.querySelector(".textP").innerHTML="";
-    document.querySelector(".textR").innerHTML="";
-    document.querySelector(".textC").innerHTML="";
-   
+    if (document.querySelector(".textP")) {
+        document.querySelector(".textP").innerHTML = "";
+    }
+    if (document.querySelector(".textR")) {
+        document.querySelector(".textR").innerHTML = "";
+    }
+    if (document.querySelector(".textC")) {
+        document.querySelector(".textC").innerHTML = "";
+    }
 
-    txtP.parentNode.removeChild(txtP);
-    txtR.parentNode.removeChild(txtR);
-    txtC.parentNode.removeChild(txtC);
+    if (document.querySelector(".textP")) {
+        txtP.parentNode.removeChild(txtP);
+    }
+    if (document.querySelector(".textR")) {
+        txtR.parentNode.removeChild(txtR);
+    }
+    if (document.querySelector(".textC")) {
+        txtC.parentNode.removeChild(txtC);
+    }
+
     txtPS.parentNode.removeChild(txtPS);
     correct.parentNode.removeChild(correct);
     buttonH.parentNode.removeChild(buttonH);
     buttonR.parentNode.removeChild(buttonR);
-    context.clearRect(0, 0, 400, 400);
-    lives=6;
-    document.querySelector('.start').style.display="block";
 
+    context.clearRect(0, 0, 400, 400);
+
+    lives = 6;
+
+    document.querySelector('.start').style.display = "block";
 }
 
-function hint(){
-    randomElement=randomNoRepeats();
+function hint() {
+    randomElement = randomNoRepeats();
     var randomElement = randomNoRepeats();
-    var hintC=randomElement();
-    
+    var hintC = randomElement();
+
     for (i = 0; i < correctArr.length; i++) {
         if (correctArr[i] === hintC) {
             var ad = 1;
             break;
         }
-    }if(ad!=1){
+    } if (ad != 1) {
         displayR(hintC);
     }
-    
+
 }
 function randomNoRepeats() {
-    let selectedNum=arr[selected].split('');
+    let selectedNum = arr[selected].split('');
     var copy = selectedNum.slice(0);
-  
-    return function(){
-      if (copy.length < 1) { copy = selectedNum.slice(0); }
-      var index = Math.floor(Math.random() * copy.length);
-      var item = copy[index];
-      copy.splice(index, 1);
-      return item;
+
+    return function () {
+        if (copy.length < 1) { copy = selectedNum.slice(0); }
+        var index = Math.floor(Math.random() * copy.length);
+        var item = copy[index];
+        copy.splice(index, 1);
+        return item;
     };
-  }
+}
 
- function canvas(){
-
+function canvas() {
     myStickman = document.getElementById("stickman");
     context = myStickman.getContext('2d');
     context.beginPath();
     context.strokeStyle = "#000";
     context.lineWidth = 2;
-  };
- function head(){
+};
+
+function head() {
     myStickman = document.getElementById("stickman");
     context = myStickman.getContext('2d');
     context.beginPath();
-    context.arc(60, 25, 10, 0, Math.PI*2, true);
+    context.arc(60, 25, 10, 0, Math.PI * 2, true);
     context.stroke();
-  }
-  
- function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
-  
-  context.moveTo($pathFromx, $pathFromy);
-  context.lineTo($pathTox, $pathToy);
-  context.stroke(); 
 }
 
- function frame1() {
-   draw (0, 150, 150, 150);
- };
- 
-  function frame2 () {
-   draw (10, 0, 10, 600);
- };
+function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke();
+}
 
-  function frame3() {
-   draw (0, 5, 70, 5);
- };
+function frame1() {
+    draw(0, 150, 150, 150);
+};
 
- function frame4 () {
-   draw (60, 5, 60, 15);
- };
+function frame2() {
+    draw(10, 0, 10, 600);
+};
 
- function torso () {
-   draw (60, 36, 60, 70);
- };
+function frame3() {
+    draw(0, 5, 70, 5);
+};
 
-  function rightArm() {
-   draw (60, 46, 100, 50);
- };
+function frame4() {
+    draw(60, 5, 60, 15);
+};
 
- function leftArm () {
-   draw (60, 46, 20, 50);
- };
+function torso() {
+    draw(60, 36, 60, 70);
+};
 
- function rightLeg () {
-   draw (60, 70, 100, 100);
- };
+function rightArm() {
+    draw(60, 46, 100, 50);
+};
 
-  function leftLeg() {
-   draw (60, 70, 20, 100);
- };
+function leftArm() {
+    draw(60, 46, 20, 50);
+};
 
- const drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+function rightLeg() {
+    draw(60, 70, 100, 100);
+};
+
+function leftLeg() {
+    draw(60, 70, 20, 100);
+};
+
+const drawArray = [rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2, frame1];
 
