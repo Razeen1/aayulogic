cf74f7fd688b5c59debdd2a781a13b3b91a9e024
rajin <rz.bhuju12@gmail.com>
2023-06-08 17:29:21 +0545

    Vue app

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="Text" v-model="newRow.name" placeholder="Enter Name"/>
+  <input type="Text" v-model="newRow.color" placeholder="Enter Color"/>
+  <input type="Text" v-model="newRow.location" placeholder="Enter Address"/>
+  <button @click="enterData" >Add</button>
+  <Button :person="person" />
+   <!-- <p>{{ person }}</p> -->
+</template>
+
+<script>
+import Button from './button.vue';
+export default {
+  name: 'App',
+  data(){
+    return{
+      person:[],
+      newRow: { name: "", color:"",location: "" },
+    }
+  },
+  components: {
+    Button
+  },
+  methods:{
+    enterData(){
+     
+            this.person.push({ ...this.newRow });
+            this.newRow.name = "";
+            this.newRow.color = "";
+            this.newRow.location = "";
+    }
+  }
+}
+</script>
---
+++ hello-world/src/components/button.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="table">
+   <table style="">
+    <tr>
+        <th>
+            Name
+        </th>
+        <th>
+            Address
+        </th>
+    </tr>
+    <tr v-for="per in person" :key="per.name" :style="{color:per.color}">
+      <td>{{per.name}}</td>
+      <td>{{ per.location }}</td>
+    </tr>
+   </table>
+</div>
+  </template>
+
+  <script>
+  export default {
+    name: 'button-item',
+    props:[
+        'person',
+    ],
+    data(){
+        return {
+           
+        }
+    },
+    methods: {
+        helloWorld(){
+            this.name2="Hello " + this.name
+            this.address1="From " + this.address
+        }
+    }
+  }
+  </script>
+
+  <style >
+table, th, td {
+  border: 1px solid white;
+  border-collapse: collapse;
+  text-align: left;
+  
+}
+td {
+  background-color: #494949;
+ 
+}
+th{
+    background-color: #202020;
+    color: #ffffff;
+}
+</style>
