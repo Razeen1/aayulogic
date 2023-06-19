77ec1133282e3483dbd74a57f84753e58065530e
rajin <rz.bhuju12@gmail.com>
2023-06-19 12:04:53 +0545

    Hr website

---
+++ hrsoft/src/components/NavBar.vue
@@ -0,0 +1,77 @@
+<template>
+    <v-card style='z-index:20001'>
+        <v-layout>
+            <v-app-bar :color="bg" density='compact' flat=true fixed :class="txt">
+
+                <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"
+                    class="hidden-sm-and-up"></v-app-bar-nav-icon>
+                <v-toolbar-title class="ml-6">RealHrSoft
+                    <!-- <v-img src="../assets/logo.png" height="100%" width="30%"></v-img> -->
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+
+                <v-toolbar-items class="d-none d-sm-flex mx-6">
+                    <v-btn flat v-for="item in items" :key="item.title" :to="item.path" variant="plain" :ripple="false">
+                        {{ item.title }}
+                    </v-btn>
+                </v-toolbar-items>
+
+            </v-app-bar>
+            <v-navigation-drawer location="left" v-model="drawer" temporary style="height: 100%" color="blue-darken-1">
+                <v-list v-model="drawer" temporary>
+                    <v-list-item v-for="(item, index) in items" :key="index" :to="item.path">
+                        <v-icon left dark></v-icon>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+
+
+        </v-layout>
+    </v-card>
+</template>
+<script>
+export default {
+    name: "Nav-Bar",
+    data: () => ({
+        drawer: false,
+        group: null,
+        is_transparent: true,
+        bg: 'transparent',
+        txt: 'text-white',
+        items: [
+            { title: 'Home', path: '/' },
+            { title: 'About', path: '/about' },
+            { title: 'Contact', path: '/contact' },
+            { title: 'Feature', path: '/feature' },
+            { title: 'Blog', path: '/blog' },
+            { title: 'FAQ', path: '/faq' }
+        ],
+    }),
+    mounted() {
+        window.onscroll = () => {
+            this.changeColor();
+        };
+    },
+    methods: {
+        changeColor() {
+            if (
+                document.body.scrollTop > 100 ||
+                document.documentElement.scrollTop > 100
+            ) {
+                this.bg = 'white';
+                this.txt = 'text-blue-darken-5';
+            } else {
+                this.bg = 'transparent';
+                this.txt = 'text-white';
+            }
+        },
+    },
+    watch: {
+        group() {
+            this.drawer = false
+        },
+    },
+}
+</script>
+  
---
+++ hrsoft/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <HelloWorld></HelloWorld>
+</template>
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+export default {
+  components: { HelloWorld }
+}
+</script>
