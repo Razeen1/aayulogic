f1684bf6f82d70e7c5c525fd81b6e41529989150
rajin <rz.bhuju12@gmail.com>
2023-06-20 11:19:18 +0545

    Tailwind added

---
+++ hrtailwind/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-4xl font-bold underline mx-auto">
+    Hello world!
+  </h1>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Hello World</button>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+
+</script>
